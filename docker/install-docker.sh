aeea0f4787b6cad652eb56a8d5b7ccd5743d5878
Anton K <killarbyte@gmail.com>
2021-07-19 17:16:59 +0300

    msg

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-apt update
-apt-get install apt-transport-https ca-certificates curl nano gnupg-agent software-properties-common sudo wget git -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo apt update
+sudo apt-get install apt-transport-https ca-certificates curl nano gnupg-agent software-properties-common sudo wget git -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
