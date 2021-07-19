34c0204b30e43a57095e688de98884c186635f9b
Anton K <killarbyte@gmail.com>
2021-07-19 15:59:44 +0300

    msg

---
+++ docker/install-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update
+apt-get install apt-transport-https ca-certificates curl nano gnupg-agent software-properties-common sudo wget git -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
