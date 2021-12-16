4d431cbce2fc52fe63a0198cf50041d4632a925f
Anton K <killarbyte@gmail.com>
2021-12-16 18:00:54 +0300

    msg

---
+++ soft/Zenno/utilites/zenno_reanimator.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+
+SETLOCAL EnableExtensions
+Set Process=Zennoposter
+tasklist | Find /i "%Process%.exe" || (goto FOUND)
+
+echo Not running
+
+exit 0
+:FOUND
+:: echo Running
+:: Указываем полный путь к файлу Zennoposter.exe
+start "" "C:\Program Files (x86)\ZennoLab\RU\ZennoPoster Pro\5.16.2.0\Progs\ZennoPoster.exe" -screen 0 -clipboard -multiwindow
+exit 0
+
+
