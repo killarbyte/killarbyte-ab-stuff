c99ddec5a642cc25e4e0cbffcc5c6210afa69d3d
Anton K <killarbyte@gmail.com>
2022-01-28 18:44:34 +0300

    msg

---
+++ scripts/posts/proxy/ipv6/genproxy48.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+ipv4=ip
+portproxy=20000
+user=login
+pass=passw
+config="/etc/3proxy/3proxy.cfg"
+
+echo -ne > /etc/3proxy/3proxy.cfg
+echo -ne > /root/proxylist.txt
+echo -ne > /root/proxylist_key_collector.txt
+echo -ne > /root/xproxy.txt
+echo -ne > /root/xevil.txt
+echo "daemon" >> $config
+echo "maxconn 3000" >> $config
+echo "nserver [2606:4700:4700::1111]" >> $config
+echo "nserver [2606:4700:4700::1001]" >> $config
+echo "nserver [2001:4860:4860::8888]" >> $config
+echo "nserver [2001:4860:4860::8844]" >> $config
+echo "nserver [2a02:6b8::feed:0ff]" >> $config
+echo "nserver [2a02:6b8:0:1::feed:0ff]" >> $config
+echo "nscache 65536" >> $config
+echo "nscache6 65536" >> $config
+echo "timeouts 1 5 30 60 180 1800 15 60" >> $config
+echo "setgid 65535" >> $config
+echo "setuid 65535" >> $config
+echo "flush" >> $config
+echo "auth strong" >> $config
+echo  "users $user:CL:$pass" >> $config
+echo "allow $user" >> $config
+
+for i in `cat ip.list`; do
+    echo "proxy -6 -s0 -n -a -olSO_REUSEADDR,SO_REUSEPORT -ocTCP_TIMESTAMPS,TCP_NODELAY -osTCP_NODELAY,SO_KEEPALIVE -p$portproxy -i$ipv4 -e$i" >> $config
+    ((inc+=1))
+    ((portproxy+=1))
+	echo "$ipv4:$portproxy:$user:$pass" >> proxylist.txt
+	echo "$ipv4:$portproxy@$user:$pass;v6" >> proxylist_key_collector.txt
+	echo "$user:$pass@$ipv4:$portproxy" >> xproxy.txt
+	echo "http://$ipv4:$portproxy:$user:$pass" >> xevil.txt
+done
