662016f517e78f5e0957f73b5e34ae2920a1d577
Anton K <killarbyte@gmail.com>
2022-01-28 20:07:30 +0300

    msg

--- scripts/posts/proxy/ipv6/genproxy48.sh
+++ scripts/posts/proxy/ipv6/genproxy48.sh
@@ -17,7 +17,7 @@ echo -ne > /app/proxy/ipv6-socks5-proxy/xevil.txt
 ### cfg start ###
 echo "daemon" >> $config
 echo "maxconn 3000" >> $config
-echo "monitor /etc/3proxy/conf/3proxy.cfg" >> $config
+echo "monitor /etc/3proxy/3proxy.cfg" >> $config
 echo "nserver [2606:4700:4700::1111]" >> $config
 echo "nserver [2606:4700:4700::1001]" >> $config
 echo "nserver [2001:4860:4860::8888]" >> $config
