2326ca27d27a02bf5e87056784c1f63d431aee24
unknown <killarbyte@gmail.com>
2023-07-01 01:31:10 +0300

    msg

--- docker/Proxy/medusa-proxy/medusa-check-mini.py
+++ docker/Proxy/medusa-proxy/medusa-check-mini.py
@@ -43,27 +43,16 @@ proxies = [
 # Имя контейнера в docker-compose для перезагрузки
 container_name = "medusa-proxy"
 
-bot_token = "YOUR_TOKEN"
-chat_id = "YOUR_CHATID"
-topic_id = "YOUR_TOPICID"
-myserver_id = "YOUR_SERVERID"
-
 error_check_urls = "Возникла ошибка при проверке ссылок через прокси."
 error_restart_container = "Ошибка при перезагрузке контейнера."
 info_restart_container = "Контейнер успешно перезагружен."
 
 if not check_urls(urls, proxies):
     print(error_check_urls)
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&reply_to_message_id={topic_id}&text={error_check_urls} {myserver_id}"
-    requests.get(url)
     
     if restart_docker_container(container_name):
         print(info_restart_container)
-        url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&reply_to_message_id={topic_id}&text={info_restart_container} {myserver_id}"
-        requests.get(url)
     else:
         print(error_restart_container)
-        url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&reply_to_message_id={topic_id}&text={error_restart_container} {myserver_id}"
-        requests.get(url)
 else:
     print("Все проверочные ссылки успешно открыты.")
