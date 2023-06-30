7006b60b191bf31b90d6bf80708425f19e937458
unknown <killarbyte@gmail.com>
2023-06-30 16:00:33 +0300

    msg

--- docker/Proxy/medusa/medusa-check.py
+++ docker/Proxy/medusa/medusa-check.py
@@ -28,24 +28,35 @@ def restart_docker_container(container_name):
 # Список URL-ов для проверки
 urls = [
     "http://ip-api.com/json",
-    "http://google.com"
+    "http://eth0.me/"
 ]
 
 # Список прокси SOCKS5 или HTTP без авторизации
 proxies = [
-    "socks5://proxy1_ip:proxy1_port",
-    "socks5://proxy2_ip:proxy2_port",
+    "http://127.0.0.1:8888",
+    "http://127.0.0.1:8889",
+    "http://127.0.0.1:8890",
+    "http://127.0.0.1:8891"
     # Добавь еще прокси по аналогии, если нужно
 ]
 
 # Имя контейнера в docker-compose для перезагрузки
 container_name = "medusa-proxy"
 
+test_token = "YOUR_TEST_TOKEN"
+chat_id = "YOUR_CHAT_ID"
+topic_id = "YOUR_TOPIC_ID"
+
 if not check_urls(urls, proxies):
     print("Не удалось открыть несколько проверочных ссылок через несколько прокси, перезагрузка контейнера...")
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&reply_to_message_id={topic_id}&text=test"
+    requests.get(url)
+    
     if restart_docker_container(container_name):
         print("Контейнер успешно перезагружен.")
+        requests.get(url)
     else:
         print("Ошибка при перезагрузке контейнера.")
+        requests.get(url)
 else:
     print("Все проверочные ссылки успешно открыты.")
