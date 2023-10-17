33d7f13618ed457ae7b00dfa4a3043b90d728b7c
unknown <killarbyte@gmail.com>
2023-10-17 23:42:50 +0300

    msg

--- scripts/posts/yandex/yandex.metrika/detect-ip-metrika.js
+++ scripts/posts/yandex/yandex.metrika/detect-ip-metrika.js
@@ -1,9 +1,9 @@
 <!-- Detect IP -->
-<script type='text/javascript'>
+<script type="text/javascript">
   var userip;
   function setUserIP(ip) {
     userip = ip;
   }
 </script>
-<script src='https://api64.ipify.org?format=jsonp&amp;callback=setUserIP' type='text/javascript'/>
+<script async src="https://api64.ipify.org?format=jsonp&amp;callback=setUserIP" type="text/javascript"/>
 <!-- /Detect IP -->
