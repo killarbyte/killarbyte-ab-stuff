17a1570d334bcb2dcbf4132ccaf5df917c64c66e
unknown <killarbyte@gmail.com>
2023-10-17 16:09:45 +0300

    msg

--- scripts/posts/yandex/yandex.metrika/detect-ip-metrika-old.js
+++
@@ -1,8 +0,0 @@
-<!-- Получаем IP-адрес -->
-<script type='text/javascript'>
-  var userip;
-  function setUserIP(ip) {
-    userip = ip;
-  }
-</script>
-<script src='https://api64.ipify.org?format=jsonp&amp;callback=setUserIP' type='text/javascript'/>
