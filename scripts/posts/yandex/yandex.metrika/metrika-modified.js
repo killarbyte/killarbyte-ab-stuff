e273ad3376b26c3b105aee8dbf811f743a49e363
unknown <killarbyte@gmail.com>
2023-10-28 04:19:04 +0300

    msg

--- scripts/posts/yandex/yandex.metrika/metrika-modified.js
+++ scripts/posts/yandex/yandex.metrika/metrika-modified.js
@@ -1,1 +1,18 @@
-<!-- Yandex.Metrika counter --> <script type='text/javascript'> (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, &quot;script&quot;, &quot;https://mc.yandex.ru/metrika/tag.js&quot;, &quot;ym&quot;); ym(XXXXXXXXX, &quot;init&quot;, { clickmap:true, trackLinks:true, params:{ip: userip}, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> <noscript><div><img alt='' src='https://mc.yandex.ru/watch/XXXXXXXXX' style='position:absolute; left:-9999px;'/></div></noscript> <!-- /Yandex.Metrika counter -->
+<!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(XXXXXXXXX, "init", {
+        clickmap:true,
+        trackLinks:true,
+        params:{ip: userip},
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/XXXXXXXXX" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
